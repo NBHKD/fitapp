1ac5ded55e597ad553aa971ed0655ac1e74d4c2c
NBHKD <ad2702@outlook.com>
2026-04-24 19:10:43 +0300

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'fittrack-v1';
+const FILES = ['./'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      return caches.open(CACHE).then(c => { c.put(e.request, res.clone()); return res; });
+    }))
+  );
+});
